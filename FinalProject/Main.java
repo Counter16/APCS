8971c5f7d31f8d4e8e27386e08e5e429ec743642
Counter16 <awang16@ladueschools.net>
2022-12-12 15:59:38 +0000

    aowdinaowdinawd

--- FinalProject/Main.java
+++ FinalProject/Main.java
@@ -1,16 +1,31 @@
 package FinalProject;
+
 import java.io.FileNotFoundException;
-import java.security.SecureRandomParameters;
-public class Main{
-    public static void main(String args []) throws FileNotFoundException{
-        System.out.println("Hello. The game you will be playing will involve the usage of country and capital names. A random country will be outputted, and players will take turns naming countries/capitals that begin with the last letter of the most recent country/capital named. Every correct made results in an increase in player score, and the game ends when a player types 'surrender'.");
+
+public class Main {
+    public static void main(String args[]) throws FileNotFoundException {
+        System.out.println(
+                "Hello. The game you will be playing will involve the usage of country and capital names. A random country will be outputted, and players will take turns naming countries/capitals that begin with the last letter of the most recent country/capital named. Every correct guess made results in an increase in player score, and the game ends when a player types 'surrender'.");
         Play run = new Play();
-        run.getLoadPhrase();
         try {
-            Thread.sleep(5000);  
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+        }
         System.out.print("\033[H\033[2J");
+        System.out.println(
+                "Soon, a list of all possible countries/capitals will appear. This will give you some examples of countries/capitals that are usable. A couple things to note: \n"
+                        + "-Countries/Capitals that use hyphens have to be inputted as hyphens(Porto-Novo, Guinea-Bissau). \n"
+                        + "-Spell correctly.");
 
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException f) {
+            Thread.currentThread().interrupt();
+            System.out.print("\033[H\033[2J");
+        }
+            run.getLoadPhrase();
+
+        }
     }
-}
+
