cdbfb69f10e70078e7eb34837a61e6e413e201db
Counter16 <awang16@ladueschools.net>
2023-03-29 16:32:31 +0000

    qwertyuiop

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -1,14 +1,37 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String args []){
+        
+    }
+    public static String chickenNugget(String x){
         Scanner sc = new Scanner(System.in);
-        System.out.println("What is your equation that needs to be solved?");
+        System.out.println("What is your equation that needs to be solved? (Type without any spaces or you're a loser)");
         String chicken = sc.nextLine();
-        String[] array = chicken.split(" ");
-        for (int i = 0; i < array.length; i++){
-            System.out.println(array[i]);
+        ArrayList<String> array = new ArrayList<String>(Arrays.asList(chicken.split()));
+        String[] fart = chicken.split("");
+        for (int i = 0; i < array.size(); i++){
+            if(fart[i].equals(" ")){
+              System.out.println("You are a loser.");
+              array.remove(i);
+            }
         }
+        for(int j = array.size(); j > 0; j--){
+          if(array.get(j).equals("(")){
+            for(int k = j; k < array.size(); k++){
+                if(array.get(j).equals(")"));
+            }
+          }
+          if(!array.get(j).equals("(")){
+            solve();
+          }
+
+          }
+    }
+    public static solve(String x){
+
+    }
     }
 }
