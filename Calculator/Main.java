ae8a8b14dc641902c1a12707cffe141ae4da92f0
Counter16 <awang16@ladueschools.net>
2023-04-19 14:52:55 +0000

    qawsedrftgyhujkl

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -1,44 +1,126 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class Main {
-    public static void main(String args []){
-      Scanner sc = new Scanner(System.in);
-      System.out.println("What is your equation that needs to be solved? (Type without any spaces or you're a loser)");
-      String chicken = sc.nextLine();
-      chickenNugget(chicken);
-    }
-    public static String chickenNugget(String chicken){
-        ArrayList<String> array = new ArrayList<String>();
-        String[] fart = chicken.split("");
-        for (int i = 0; i < array.size(); i++){
-            if(fart[i].equals(" ")){
-              System.out.println("You are a loser.");
-              array.remove(i);
-            }
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("List an expression you need to solve. Don't include any spaces.");
+    chickenNugget(sc.nextLine());
+    sc.close();
+  }
+  
+  public static int chickenNugget(String chicken){
+    ArrayList<String> elements = new ArrayList<String>();
+    boolean addInt = false;
+    boolean addOther = true;
+    for (int i = 0; i < chicken.length(); i++) {
+      if (Character.isDigit(chicken.charAt(i))) {
+        addOther = false;
+        if (addInt) {
+          elements.set(elements.size() - 1, elements.get(elements.size() - 1) + chicken.substring(i, i + 1));
         }
-        for(int j = array.size(); j > 0; j--){
-          if(array.get(j).equals("(")){
-            for(int k = j; k < array.size(); k++){
-                if(array.get(j).equals(")"));
-            }
-          }
-          if(!array.get(j).equals("(")){
-            solve();
-          }
-
-          }
-    }
-    
-    }
-    public static solve(String x){
-      while(x.contains("^")){
-        int i = x.indexOf("^"); 
-        while((i - 2) >= 0 && x.charAt(i-2) == '(' && x.charAt(i+2) == ')'){
-           int y = x.charAt(i-1)^x.charAt(i+1);
-           String z = 
+        else {
+          elements.add(chicken.substring(i, i + 1));
+          addInt = true;
+        } 
+      }
+      else {
+        addInt = false;
+        elements.add(chicken.substring(i, i + 1));
+        if (addOther && chicken.charAt(i) == '-') {
+          addInt = true;
+        }
+        else {
+          addOther = true;
         }
       }
     }
+    int count = 0;
+    System.out.println(evaluate(elements, count));
+    return 0;
+  }
+  public static int evaluate(ArrayList<String> elements, int count){  
+    if (elements.contains("(")) {
+      int i1 = 0;
+      int i2 = 0;
+      for (int i = 0; i < elements.size(); i++) {
+        if (elements.get(i).equals("(")) {
+          i1 = i;
+        }
+        else if (elements.get(i).equals(")")) {
+          i2 = i;
+          break;
+        }
+      }
+      elements.add(i1, String.valueOf(chickenNugget(String.join("", elements.subList(i1 + 1, i2)))));
+      for (int i = 0; i < i2 - i1 + 1; i++) {
+        elements.remove(i1+1);
+      }
+      chickenNugget(String.join("",elements));
     }
+    else if (elements.contains("^")) {
+      int i = elements.indexOf("^");
+      int x = (int) Math.pow(Integer.parseInt(elements.get(i - 1)), Integer.parseInt(elements.get(i + 1)));
+      elements.add(i - 1, String.valueOf(x));
+      elements.remove(i);
+      elements.remove(i);
+      elements.remove(i);
+      chickenNugget(String.join("",elements));
+      count += x;
+    }  
+    else if (elements.contains("*") || elements.contains("/")) {
+      int i;
+      if (elements.indexOf("*") == -1) {
+        i = elements.indexOf("/");
+      }
+      else if (elements.indexOf("/") == -1) {
+        i = elements.indexOf("*");
+      }
+      else {
+        i = Math.min(elements.indexOf("*"), elements.indexOf("/"));
+      }
+      int x;
+      if (elements.get(i).equals("*")) {
+        x = Integer.parseInt(elements.get(i - 1)) * Integer.parseInt(elements.get(i + 1));
+        count += x;
+      }
+      else {
+        x = Integer.parseInt(elements.get(i - 1)) / Integer.parseInt(elements.get(i + 1));
+        count += x;
+      }
+      elements.add(i - 1, String.valueOf(x));
+      elements.remove(i); elements.remove(i); elements.remove(i);
+      chickenNugget(String.join("",elements));
+    }
+    else if (elements.contains("+") || elements.contains("-")) {
+      int i;
+      if (elements.indexOf("+") == -1) {
+        i = elements.indexOf("-");
+      }
+      else if (elements.indexOf("-") == -1) {
+        i = elements.indexOf("+");
+      }
+      else {
+        i = Math.min(elements.indexOf("+"), elements.indexOf("-"));
+      }
+      int x;
+      if (elements.get(i).equals("+")) {
+        x = Integer.parseInt(elements.get(i - 1)) + Integer.parseInt(elements.get(i + 1));
+        count += x;
+      }
+      else {
+        x = Integer.parseInt(elements.get(i - 1)) - Integer.parseInt(elements.get(i + 1));
+        count += x;
+      }
+      elements.add(i - 1, String.valueOf(x));
+      elements.remove(i); elements.remove(i); elements.remove(i);
+      chickenNugget(String.join("",elements));
+    }
+    else {
+      return Integer.parseInt(elements.get(0));
+      //System.out.println(count);
+    }
+    return 0;
+  }
+}
+
